99d9c70bf3ec1983c8f51267fa469da75f8729ef
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-04-25 21:28:42 -0300

    [ :sparkles: ] | create: strategy - account balancer (load_balancer::module)

--- rinha/load_balancer/src/model/mod.rs
+++ rinha/load_balancer/src/model/mod.rs
@@ -1,10 +1,10 @@
-use std::sync::{atomic::AtomicUsize, Arc};
+use std::sync::Arc;
 
 use axum::{body::Body, extract::Request};
 use hyper_util::client::legacy::{connect::HttpConnector, Client};
 
 pub trait LoadBalancer {
-    pub fn next_server(&self, req: &Request) -> String;
+    fn next_server(&self, req: &Request) -> String;
 }
 
 #[derive(Clone)]
--- rinha/load_balancer/src/strategy/mod.rs
+++ rinha/load_balancer/src/strategy/mod.rs
@@ -1,6 +1,9 @@
-use std::sync::{
-    atomic::{AtomicUsize, Ordering},
-    Arc,
+use std::{
+    hash::{DefaultHasher, Hash, Hasher},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
 };
 
 use axum::extract::Request;
@@ -16,5 +19,21 @@ impl LoadBalancer for RoundRobin {
     fn next_server(&self, req: &Request) -> String {
         let count = self.req_counter.fetch_add(1, Ordering::Relaxed);
         self.addrs[count % self.addrs.len()].clone()
+    }
+}
+
+pub struct RinhaAccountBalancer {
+    pub addrs: Vec<String>,
+}
+
+impl LoadBalancer for RinhaAccountBalancer {
+    fn next_server(&self, req: &Request) -> String {
+        let path = req.uri().path();
+        let hash = {
+            let mut hasher = DefaultHasher::new();
+            path.hash(&mut hasher);
+            hasher.finish() as usize
+        };
+        self.addrs[hash % self.addrs.len()].to_string()
     }
 }
