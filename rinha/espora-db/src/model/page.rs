a56d11334350485d1b23c242e4ee0f0f528bf5bc
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-06 22:44:38 -0300

    [ :sparkles: ] | create: model - Page (app::espora)

--- rinha/espora-db/src/model/page.rs
+++ rinha/espora-db/src/model/page.rs
@@ -2,3 +2,14 @@ pub struct Page<const ROW_SIZE: usize> {
     data: Vec<u8>,
     free: usize,
 }
+
+pub const PAGE_SIZE: usize = 4096;
+
+impl<const ROW_SIZE: usize> Page<ROW_SIZE> {
+    pub fn new() -> Self {
+        Self {
+            data: Vec::with_capacity(PAGE_SIZE),
+            free: PAGE_SIZE,
+        }
+    }
+}
