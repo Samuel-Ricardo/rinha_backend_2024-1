f95ae5a6a30f9de057ea538d7cd0c94cbf210bec
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-06 22:08:59 -0300

    [ :goal_net: ] | catch: errors (app::module)

---
+++ rinha/espora-db/src/error.rs
@@ -0,0 +1,23 @@
+use std::{error, fmt, io};
+
+pub enum Error {
+    Io(io::Error),
+    Serialization(Box<dyn error::Error + Send + Sync>),
+}
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            Self::Io(err) => write!(f, "{err}"),
+            Self::Serialization(err) => write!(f, "{err}"),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Self {
+        Self::Io(err)
+    }
+}
