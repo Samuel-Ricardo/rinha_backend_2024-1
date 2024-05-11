37fbf0df9487e88547d88668350329489afed37d
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-10 21:50:11 -0300

    [ :sparkles: ] | create: insert - database  (espora::model)

--- rinha/espora-db/src/model/database.rs
+++ rinha/espora-db/src/model/database.rs
@@ -1,6 +1,6 @@
 use std::{
     fs::{File, OpenOptions},
-    io::{self, Read, Seek},
+    io::{self, Read, Seek, Write},
     marker::PhantomData,
     os::windows::fs::FileExt,
     path::Path,
@@ -56,5 +56,33 @@ impl<const ROW_SIZE: usize, T: Serialize + DeserializeOwned> Db<T, ROW_SIZE> {
             sync_writes: Some(Duration::from_secs(0)),
             data: PhantomData,
         })
+    }
+
+    pub fn insert(&mut self, row: T) -> DbResult<()> {
+        self.current_page.insert(row)?;
+
+        self.writer.write_all(
+            &[
+                self.current_page.as_ref(),
+                &vec![0; PAGE_SIZE - self.current_page.as_ref().len()],
+            ]
+            .concat(),
+        )?;
+
+        match self.sync_writes {
+            Some(interval) if self.last_sync.elapsed() > interval => {
+                self.writer.sync_data()?;
+                self.last_sync = Instant::now();
+            }
+            _ => {}
+        }
+
+        if self.current_page.available_rows() == 0 {
+            self.current_page = Page::new();
+        } else {
+            self.writer.seek(io::SeekFrom::End(-(PAGE_SIZE as i64)))?;
+        }
+
+        Ok(())
     }
 }
