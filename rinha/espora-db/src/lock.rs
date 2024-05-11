b1eee015a555e7d72e34eff1cfdb5a92d1396e82
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-10 22:49:45 -0300

    [ :heavy_plus_sign: ] | add: dep - winapi > fileapi (os)

--- rinha/espora-db/src/lock.rs
+++ rinha/espora-db/src/lock.rs
@@ -3,13 +3,13 @@ use winapi::ctypes::c_void;
 use winapi::um::synchapi::ReleaseMutex;
 
 pub struct LockHandle {
-    pub(crate) handle: RawHandle,
+    pub(crate) fd: RawHandle,
 }
 
 impl Drop for LockHandle {
     fn drop(&mut self) {
         unsafe {
-            ReleaseMutex(self.handle as *mut c_void);
+            ReleaseMutex(self.fd as *mut c_void);
         }
     }
 }
