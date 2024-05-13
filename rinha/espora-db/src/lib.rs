40309a3e7599fde409cc7e51d9a89d3a0e2d9974
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-12 21:29:50 -0300

    [ :card_file_box: ] | create: model - database > builder (spora::model::tokio)

--- rinha/espora-db/src/lib.rs
+++ rinha/espora-db/src/lib.rs
@@ -2,3 +2,4 @@ pub mod error;
 pub mod lock;
 pub mod model;
 mod test;
+pub mod tokio;
--- rinha/espora-db/src/model/tokio/database.rs
+++ rinha/espora-db/src/model/tokio/database.rs
@@ -1,10 +1,12 @@
 use std::marker::PhantomData;
 
+use serde::{de::DeserializeOwned, Serialize};
 use tokio::{
     fs::File,
     time::{Duration, Instant},
 };
 
+use crate::model::builder::Builder;
 use crate::model::page::Page;
 
 pub struct Db<T, const ROW_SIZE: usize> {
@@ -14,4 +16,10 @@ pub struct Db<T, const ROW_SIZE: usize> {
     last_sync: Instant,
     pub(crate) sync_writes: Option<Duration>,
     data: PhantomData<T>,
+}
+
+impl<const ROW_SIZE: usize, T: Serialize + DeserializeOwned> Db<T, ROW_SIZE> {
+    pub fn builder() -> Builder {
+        Builder::default()
+    }
 }
