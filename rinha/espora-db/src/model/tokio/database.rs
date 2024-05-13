464cab3c2a226de0ff51d26b9cbf9139b046e0d4
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-05-12 22:52:53 -0300

    [ :card_file_box: ] | create: database - lock_writes (spora::model::tokio)

--- rinha/espora-db/src/model/tokio/database.rs
+++ rinha/espora-db/src/model/tokio/database.rs
@@ -1,19 +1,29 @@
-use std::{marker::PhantomData, path::Path};
+use std::{
+    marker::PhantomData,
+    os::windows::io::{AsHandle, AsRawHandle},
+    path::Path,
+    sync::Arc,
+};
 
 use serde::{de::DeserializeOwned, Serialize};
 use tokio::{
     fs::{File, OpenOptions},
-    io::{self, AsyncReadExt, AsyncSeekExt},
+    io::{self, AsyncReadExt, AsyncSeekExt, AsyncWriteExt},
+    sync::oneshot,
+    task,
     time::{Duration, Instant},
 };
 
-use crate::model::page::Page;
-use crate::model::{builder::Builder, page::PAGE_SIZE};
+use crate::model::{database::DbResult, page::Page};
+use crate::{
+    lock::LockHandle,
+    model::{builder::Builder, page::PAGE_SIZE},
+};
 
 pub struct Db<T, const ROW_SIZE: usize> {
     current_page: Page<ROW_SIZE>,
     reader: File,
-    write: File,
+    writer: File,
     last_sync: Instant,
     pub(crate) sync_writes: Option<Duration>,
     data: PhantomData<T>,
@@ -49,10 +59,55 @@ impl<const ROW_SIZE: usize, T: Serialize + DeserializeOwned> Db<T, ROW_SIZE> {
         Ok(Self {
             current_page,
             reader: File::open(&path).await?,
-            write: file,
+            writer: file,
             last_sync: Instant::now(),
             sync_writes: Some(Duration::from_secs(0)),
             data: PhantomData,
         })
+    }
+
+    pub async fn insert(&mut self, row: T) -> DbResult<()> {
+        self.current_page.insert(row)?;
+
+        self.writer
+            .write_all(
+                &[
+                    self.current_page.as_ref(),
+                    &vec![0; PAGE_SIZE - self.current_page.len()],
+                ]
+                .concat(),
+            )
+            .await?;
+
+        match self.sync_writes {
+            Some(interval) if self.last_sync.elapsed() > interval => {
+                self.writer.sync_data().await?;
+                self.last_sync = Instant::now();
+            }
+            _ => {}
+        }
+
+        if self.current_page.available_rows() == 0 {
+            self.current_page = Page::new();
+        } else {
+            self.writer
+                .seek(io::SeekFrom::End(-(PAGE_SIZE as i64)))
+                .await?;
+        }
+
+        Ok(())
+    }
+
+    pub async fn lock_writes(&mut self) -> DbResult<LockHandle> {
+        let (tx, rx) = oneshot::channel();
+        let fd = self.writer.as_raw_fd();
+        task::spawn_blocking(move || match unsafe { libc::flock(fd, libc::LOCK_EX) } {
+            0 => tx.send(Ok(LockHandle { fd })),
+            _ => tx.send(Err(io::Error::new(
+                io::ErrorKind::Other,
+                "couldn't acquire lock",
+            ))),
+        });
+        Ok(rx.await.unwrap()?)
     }
 }
